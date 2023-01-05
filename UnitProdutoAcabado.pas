9aab9ca2f8560b46da6ddb727839b4fd23ae21b4
madsonMendes <madson.nrm@outlook.com>
2023-01-05 18:09:40 -0300

    continuacao iniciar corte

---
+++ UnitProdutoAcabado.pas
@@ -0,0 +1,24 @@
+unit UnitProdutoAcabado;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TformProdutoAcabado = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  formProdutoAcabado: TformProdutoAcabado;
+
+implementation
+
+{$R *.dfm}
+
+end.
