8ee3c31716d4141126857ebc4bde4c9e52048300
madsonMendes <madson.nrm@outlook.com>
2023-01-07 12:47:47 -0300

    grid prodAcabado+wipecode

--- UnitPrincipal.pas
+++ UnitPrincipal.pas
@@ -108,6 +108,7 @@ type
     procedure IniciarCorte1Click(Sender: TObject);
   private
     { Private declarations }
+    procedure gridViewOrdemCorte;
   public
     { Public declarations }
   end;
@@ -275,37 +276,7 @@ begin
             qyOrdemCorte.SQL.Add('ORDER BY oc.oc_id desc limit 30');
             qyOrdemCorte.Open;
       end;
-
-      gridOrdem.Columns[0].Title.Alignment:=taCenter;
-      gridOrdem.Columns[0].Title.Caption:='Nº Corte';
-      gridOrdem.Columns[1].Title.Alignment:=taCenter;
-      gridOrdem.Columns[1].Title.Caption:='Coleção';
-      gridOrdem.Columns[2].Title.Alignment:=taCenter;
-      gridOrdem.Columns[2].Title.Caption:='Situação';
-      gridOrdem.Columns[3].Title.Alignment:=taCenter;
-      gridOrdem.Columns[3].Title.Caption:='Estilista';
-      gridOrdem.Columns[4].Title.Alignment:=taCenter;
-      gridOrdem.Columns[4].Title.Caption:='Ficha Técnica';
-      gridOrdem.Columns[5].Title.Alignment:=taCenter;
-      gridOrdem.Columns[5].Title.Caption:='Ordem de Produção';
-      gridOrdem.Columns[6].Title.Alignment:=taCenter;
-      gridOrdem.Columns[6].Title.Caption:='Dias em Processo';
-      gridOrdem.Columns[7].Title.Alignment:=taCenter;
-      gridOrdem.Columns[7].Title.Caption:='Tipo';
-      gridOrdem.Columns[8].Title.Alignment:=taCenter;
-      gridOrdem.Columns[8].Title.Caption:='Referência';
-      gridOrdem.Columns[9].Title.Alignment:=taCenter;
-      gridOrdem.Columns[9].Title.Caption:='Produto Acabado';
-      gridOrdem.Columns[10].Title.Alignment:=taCenter;
-      gridOrdem.Columns[10].Title.Caption:='Inicio Ordem de Corte';
-      gridOrdem.Columns[11].Title.Alignment:=taCenter;
-      gridOrdem.Columns[11].Title.Caption:='Corte Previsto';
-      gridOrdem.Columns[12].Title.Alignment:=taCenter;
-      gridOrdem.Columns[12].Title.Caption:='Real Cortado';
-      gridOrdem.Columns[13].Title.Alignment:=taCenter;
-      gridOrdem.Columns[13].Title.Caption:='Localização Empenho';
-      gridOrdem.Columns[14].Title.Alignment:=taCenter;
-      gridOrdem.Columns[14].Title.Caption:='Observação'
+      gridViewOrdemCorte;
     end;
 end;
 
@@ -328,67 +299,12 @@ end;
 
 procedure TformPrincipal.FormShow(Sender: TObject);
 begin
-    begin
-      With dmOrdemCorte do
-      begin
-            qyOrdemCorte.Close;
-            qyOrdemCorte.SQL.Clear;
-            qyOrdemCorte.SQL.Add('SELECT');
-            qyOrdemCorte.SQL.Add('Cast(concat(lpad(cast(oc.oc_id As varchar), 7, ''0''), ''-'', lpad(cast(oc.oc_ordem As varchar), 3, ''0'') )as character varying(15))as NumerodoCorte,');
-            qyOrdemCorte.SQL.Add('cc.co_descricao, (ocs.os_nome)as situacao,');
-            qyOrdemCorte.SQL.Add('(ce.es_nome)as estilista,');
-            qyOrdemCorte.SQL.Add('oc.oc_idfichatecnica,');
-            qyOrdemCorte.SQL.Add('(SELECT op.op_id FROM ordem_producao AS op WHERE op.op_idordemcorte = oc.oc_id AND op.op_idstatus <> ''4'')as noordproducao,');
-            qyOrdemCorte.SQL.Add('(SELECT (CASE WHEN ((oc.oc_situacao = 1) AND (coalesce(ocpi.oci_id, 0) = 0) AND (coalesce(ocri.oci_id,0) = 0)) THEN');
-            qyOrdemCorte.SQL.Add('                  CURRENT_DATE - oc.oc_dtgerada::DATE');
-            qyOrdemCorte.SQL.Add('              WHEN ((oc.oc_situacao = 1) AND (coalesce(ocpi.oci_id, 0) > 0) AND (coalesce(ocri.oci_id,0) = 0)) THEN');
-            qyOrdemCorte.SQL.Add('                  CURRENT_DATE - ocpi.oci_dtlanc::DATE');
-            qyOrdemCorte.SQL.Add('              WHEN ((oc.oc_situacao = 1) AND (coalesce(ocpi.oci_id, 0) > 0) AND (coalesce(ocri.oci_id,0) > 0)) THEN');
-            qyOrdemCorte.SQL.Add('                  CURRENT_DATE - ocri.oci_dtlanc::DATE');
-            qyOrdemCorte.SQL.Add('         ELSE 0 END)');
-            qyOrdemCorte.SQL.Add('     FROM ordem_corte as oci LEFT JOIN ordem_corte_itens_previsto as ocpi on');
-            qyOrdemCorte.SQL.Add('     ((ocpi.oci_idocorte = oci.oc_id) and (ocpi.oci_situacao_id NOT IN (2, 4)))');
-            qyOrdemCorte.SQL.Add('     LEFT JOIN ordem_corte_itens_real as ocri on');
-            qyOrdemCorte.SQL.Add('     ((ocri.oci_idocorte = oci.oc_id) and (ocpi.oci_situacao_id NOT IN (2, 4)))');
-            qyOrdemCorte.SQL.Add('     WHERE oci.oc_id=oc.oc_id');
-            qyOrdemCorte.SQL.Add('     LIMIT 1');
-            qyOrdemCorte.SQL.Add(') AS diasemprocesso,');
-            qyOrdemCorte.SQL.Add('Cast(Case When oc.oc_prototipo = True Then ''Protótipo'' ELSE ''Grande Escala'' END as character varying(15)) AS tipo,');
-            qyOrdemCorte.SQL.Add('(pa.cad_idreferencia)as referencia,');
-            qyOrdemCorte.SQL.Add('(pa.cad_descricao)as produtoacabado,');
-            qyOrdemCorte.SQL.Add('(oc.oc_dtgerada)as datagerada,');
-            qyOrdemCorte.SQL.Add('(SELECT oci_dtlanc FROM ordem_corte_itens_previsto WHERE oci_idocorte=oc.oc_id AND oci_situacao_id <> 2 LIMIT 1) as datagradeprevisto,');
-            qyOrdemCorte.SQL.Add('(SELECT oci_dtlanc FROM ordem_corte_itens_real WHERE oci_idocorte=oc.oc_id AND oci_situacao_id <> 2 LIMIT 1) as datagradereal,');
-            qyOrdemCorte.SQL.Add('  Cast(CASE');
-            qyOrdemCorte.SQL.Add('      WHEN (SELECT emp_tipo FROM controle_empenho WHERE emp_idordemcorte= oc.oc_id And emp_situacao <>''C'' LIMIT 1) IS NULL THEN ''NÃO EMPENHADO''');
-            qyOrdemCorte.SQL.Add('      ELSE');
-            qyOrdemCorte.SQL.Add('          (CASE');
-            qyOrdemCorte.SQL.Add('              WHEN (SELECT emp_tipo FROM controle_empenho');
-            qyOrdemCorte.SQL.Add('                          WHERE emp_idordemcorte=oc.oc_id');
-            qyOrdemCorte.SQL.Add('                          AND emp_situacao IN (''N'', ''P'')');
-            qyOrdemCorte.SQL.Add('                          AND emp_mod = ''0''');
-            qyOrdemCorte.SQL.Add('                          ORDER BY emp_id DESC LIMIT 1)= ''S'' THEN ''PLANEJAMENTO => ALMOXARIFADO''');
-            qyOrdemCorte.SQL.Add('              WHEN (SELECT emp_tipo FROM controle_empenho');
-            qyOrdemCorte.SQL.Add('                          WHERE emp_idordemcorte=oc.oc_id');
-            qyOrdemCorte.SQL.Add('                          AND emp_situacao=''N''');
-            qyOrdemCorte.SQL.Add('                          AND emp_mod = ''1''');
-            qyOrdemCorte.SQL.Add('                          ORDER BY emp_id DESC LIMIT 1)= ''E'' THEN ''ALMOXARIFADO => CORTE''');
-            qyOrdemCorte.SQL.Add('              WHEN (SELECT emp_tipo FROM controle_empenho');
-            qyOrdemCorte.SQL.Add('                          WHERE emp_idordemcorte=oc.oc_id');
-            qyOrdemCorte.SQL.Add('                          AND emp_situacao= ''P''');
-            qyOrdemCorte.SQL.Add('                          AND emp_mod = ''1''');
-            qyOrdemCorte.SQL.Add('                          ORDER BY emp_id DESC LIMIT 1) =''E'' THEN ''RECEBIDO NO CORTE''');
-            qyOrdemCorte.SQL.Add('              END)');
-            qyOrdemCorte.SQL.Add('  END as character varying(30)) AS processoempenho,');
-            qyOrdemCorte.SQL.Add('oc.oc_observacao FROM ordem_corte as oc JOIN produto_acabado as pa on pa.cad_id = oc.oc_idcodprodutoacabado LEFT JOIN colecao as cc on');
-            qyOrdemCorte.SQL.Add('cc.co_id = pa.cad_idcolecao LEFT JOIN cadastro_estilista as ce on pa.cad_idestilista = ce.es_id LEFT JOIN ordem_corte_situacao as ocs on');
-            qyOrdemCorte.SQL.Add('ocs.os_id = oc.oc_situacao WHERE oc.oc_situacao <> ''2'' AND not EXISTS (SELECT * FROM ordem_producao AS op');
-            qyOrdemCorte.SQL.Add('                           WHERE op.op_idordemcorte=oc.oc_id and op.op_idstatus <> 4)');
-            qyOrdemCorte.SQL.Add('ORDER BY oc.oc_id desc limit 30');
-            qyOrdemCorte.Open;
-            comboTipo.Items.Add('');
-      end;
-    end;
+    dmOrdemCorte.qyOrdemCorte.Active:=true;
+    gridViewOrdemCorte;
+end;
+
+procedure TformPrincipal.gridViewOrdemCorte;
+begin
     gridOrdem.Columns[0].Title.Alignment:=taCenter;
     gridOrdem.Columns[0].Title.Caption:='Nº Corte';
     gridOrdem.Columns[1].Title.Alignment:=taCenter;
